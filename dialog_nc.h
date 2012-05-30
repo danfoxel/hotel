8ef6ce118ac42ab18fae55389d99688d36eda976
Daniel Dyakov <dan-d@ya.ru>
2012-05-30 13:56:36 +0400

    Revert "Revert "test""
    This reverts commit d5725bbfc15742cf6e15069e80cad06497a3df15.

---
+++ dialog_nc.h
@@ -0,0 +1,38 @@
+#ifndef DIALOG_NC_H
+#define DIALOG_NC_H
+
+#include <QDialog>
+#include "ui_dialog_nc.h"
+
+class Dialog_nc : public QDialog
+{
+    Q_OBJECT
+    
+public:
+    Dialog_nc(QWidget *parent = 0);
+    ~Dialog_nc();
+
+
+
+
+     QString Client_fam() const;
+     QString Client_nam() const;
+     QString Client_otc() const;
+     QString Client_pass() const;
+     QString Client_passvid() const;
+     QString Client_passplvid() const;
+     QString Client_in() const;
+     QString Client_out() const;
+     QString Client_gender()const;
+
+  //   void newquery(const QString &fam,const QString &nam,const  QString &otc,const QString &pass,
+    //                     const QString &passvid,const QString &passplvid,const QString &in,const QString &gender);
+
+private slots:
+        void on_pushButton_clicked();
+
+private:
+    Ui::Dialog_nc ui;
+};
+
+#endif // DIALOG_NC_H
